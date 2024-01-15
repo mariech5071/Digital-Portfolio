1a32d23bb1bedc449e959126006c517c06684b6b
mariech5071 <125594493+mariech5071@users.noreply.github.com>
2024-01-15 10:56:29 -0600

    transitions changed

--- digital-portfolio/Components/About1.tsx
+++ digital-portfolio/Components/About1.tsx
@@ -1,44 +1,21 @@
 import React, { useEffect, useState,useRef } from 'react';
-import { motion } from 'framer-motion';
 import Image from 'next/image';
 
 const About1 = () => {
-    const [inView, setInView] = useState(false);
-    
-    
-
-    useEffect(() => {
-        let lastScrollTop = 0;
-        const handleScroll = () => {
-            const firstDivElement = document.getElementById('firstDiv');
-            if (firstDivElement) {
-                const st = window.scrollY || document.documentElement.scrollTop;
-                if (st > lastScrollTop){
-                    const rect = firstDivElement.getBoundingClientRect();
-                    const isInView = rect.top < window.innerHeight;
-                    setInView(isInView);
-                }
-                lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
-            }
-        };
-
-        window.addEventListener('scroll', handleScroll);
-        return () => window.removeEventListener('scroll', handleScroll);
-    }, []);
 
     return (
         <div id='about' className='min-h-screen flex flex-col sm:flex-row justify-center items-center py-4'>
-            <motion.div id='firstDiv' className='w-full sm:w-1/2 p-4 order-last sm:order-first' initial={{ x: '-100vw' }} animate={{ x: inView ? 0 : '-100vw' }} transition={{ duration: 0.5 }}>
+            <div id='firstDiv' className='w-full sm:w-1/2 p-4 order-last sm:order-first'>
                 <Image src="/images/Profile.png" alt="About" className="h-80 rounded-lg mx-auto" />
-            </motion.div>
-            <motion.div className='w-full sm:w-1/2 p-4' initial={{ x: '100vw' }} animate={{ x: inView ? 0 : '100vw' }} transition={{ duration: 0.5 }}>
+            </div>
+            <div className='w-full sm:w-1/2 p-4'>
                 <h1 className='text-center mb-4'>About</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod eros sed lorem rutrum, at posuere lorem tincidunt. Nullam.
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod eros sed lorem rutrum, at posuere lorem tincidunt. NullamLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod eros sed lorem rutrum, at posuere lorem tincidunt. Nullam
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod eros sed lorem rutrum, at posuere lorem tincidunt. NullamLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod eros sed lorem rutrum, at posuere lorem tincidunt. Nullam
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod eros sed lorem rutrum, at posuere lorem tincidunt. NullamLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod eros sed lorem rutrum, at posuere lorem tincidunt. Nullam
                 </p>
-            </motion.div>
+            </div>
         </div>
     )
 }
--- digital-portfolio/Components/Projects1.tsx
+++ digital-portfolio/Components/Projects1.tsx
@@ -1,88 +1,52 @@
-import React, { useState, useEffect } from 'react';
+
 import { motion } from 'framer-motion';
 
 
-
-
 const Projects1 = () => {
-    const [inView, setInView] = useState(false);
-    const [inView1, setInView1] = useState(false);
-    const [inView2, setInView2] = useState(false);
-    const [inView3, setInView3] = useState(false);
-
-    const handleScroll = () => {
-        if (typeof document !== 'undefined') {
-        const divElement = document.getElementById('projects');
-        const imgElement1 = document.getElementById('img1');
-        const imgElement2 = document.getElementById('img2');
-        const imgElement3 = document.getElementById('img3');
-        if (divElement && imgElement1 && imgElement2 && imgElement3) {
-            const rect = divElement.getBoundingClientRect();
-            const rect1 = imgElement1.getBoundingClientRect();
-            const rect2 = imgElement2.getBoundingClientRect();
-            const rect3 = imgElement3.getBoundingClientRect();
-            const isInView = rect.top < window.innerHeight;
-            const isInView1 = rect1.top < window.innerHeight;
-            const isInView2 = rect2.top < window.innerHeight;
-            const isInView3 = rect3.top < window.innerHeight;
-            setInView(isInView);
-            setInView1(isInView1);
-            setInView2(isInView2);
-            setInView3(isInView3);
-        }
-        }
-    };
-
-    useEffect(() => {
-        window.addEventListener('scroll', handleScroll);
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, []);
-
+    
     return (
         <div id='projects' className='bg-custom-orange-lighter min-h-screen flex justify-center items-center flex-col'>
-            <motion.h1 className='mx-auto text-center mb-4 pt-6' initial={{ y: '100vh' }} animate={{ y: inView ? 0 : '100vh' }} transition={{ duration: 0.5 }}>
+            <h1 className='mx-auto text-center mb-4 pt-6'>
                 Projects
-            </motion.h1>
+            </h1>
             <div className="flex flex-col sm:flex-row sm:space-x-20 justify-center items-center space-y-8 sm:space-y-0 pb-6">
                 <div className="w-full sm:w-1/2 xl:w-1/4" id='img1'>
-                <motion.img  whileHover={{ scale: 1.1 }} src="/images/Arcane-studio.png" alt="Project 1" className="w-full h-48 sm:h-48 object-contain mx-auto rounded-lg" initial={{ y: '100vh' }} animate={{ y: inView1 ? 0 : '100vh' }} transition={{ duration: 0.5 }} />
+                <motion.img whileHover={{ scale: 1.1 }} src="/images/Arcane-studio.png" alt="Project 1" className="w-full h-48 sm:h-48 object-contain mx-auto rounded-lg" />
                     <div className="flex items-center justify-between">
-                    <motion.p className="font-medium mt-3" initial={{ y: '100vh' }} animate={{ y: inView1 ? 0 : '100vh' }} transition={{ duration: 0.5 }}>
+                    <p className="font-medium mt-3">
                     Here is project 1
-                    </motion.p>
-                        <motion.button className="w-8 h-8 bg-white rounded-full flex items-center justify-center" initial={{ y: '100vh' }} animate={{ y: inView1 ? 0 : '100vh' }} transition={{ duration: 0.5 }}>
+                    </p>
+                        <button className="w-8 h-8 bg-white rounded-full flex items-center justify-center">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="black" className="w-4 h-4">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M14 5l7 7m0 0l-7 7m7-7H3" />
                             </svg>
-                        </motion.button>
+                        </button>
                     </div>
                 </div>
                 <div className="w-full sm:w-1/2 xl:w-1/4" id='img2'>
-                <motion.img whileHover={{ scale: 1.1 }} src="/images/Arcane-studio.png" alt="Project 1" className="w-full h-48 sm:h-48 object-contain mx-auto rounded-lg" initial={{ y: '100vh' }} animate={{ y: inView2 ? 0 : '100vh' }} transition={{ duration: 0.5 }} />
+                <motion.img whileHover={{ scale: 1.1 }} src="/images/Arcane-studio.png" alt="Project 1" className="w-full h-48 sm:h-48 object-contain mx-auto rounded-lg"/>
                     <div className="flex items-center justify-between">
-                    <motion.p className="font-medium mt-3" initial={{ y: '100vh' }} animate={{ y: inView2 ? 0 : '100vh' }} transition={{ duration: 0.5 }}>
+                    <p className="font-medium mt-3">
                     Here is project 1
-                    </motion.p>
-                        <motion.button className="w-8 h-8 bg-white rounded-full flex items-center justify-center" initial={{ y: '100vh' }} animate={{ y: inView2 ? 0 : '100vh' }} transition={{ duration: 0.5 }}>
+                    </p>
+                        <button className="w-8 h-8 bg-white rounded-full flex items-center justify-center">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="black" className="w-4 h-4">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M14 5l7 7m0 0l-7 7m7-7H3" />
                             </svg>
-                        </motion.button>
+                        </button>
                     </div>
                 </div>
                 <div className="w-full sm:w-1/2 xl:w-1/4" id='img3'>
-                <motion.img whileHover={{ scale: 1.1 }} src="/images/Arcane-studio.png" alt="Project 1" className="w-full h-48 sm:h-48 object-contain mx-auto rounded-lg" initial={{ y: '100vh' }} animate={{ y: inView3 ? 0 : '100vh' }} transition={{ duration: 0.5 }} />
+                <motion.img whileHover={{ scale: 1.1 }} src="/images/Arcane-studio.png" alt="Project 1" className="w-full h-48 sm:h-48 object-contain mx-auto rounded-lg"/>
                     <div className="flex items-center justify-between">
-                    <motion.p className="font-medium mt-3" initial={{ y: '100vh' }} animate={{ y: inView3 ? 0 : '100vh' }} transition={{ duration: 0.5 }}>
+                    <p className="font-medium mt-3">
                     Here is project 1
-                    </motion.p>
-                        <motion.button className="w-8 h-8 bg-white rounded-full flex items-center justify-center" initial={{ y: '100vh' }} animate={{ y: inView3 ? 0 : '100vh' }} transition={{ duration: 0.5 }}>
+                    </p>
+                        <button className="w-8 h-8 bg-white rounded-full flex items-center justify-center">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="black" className="w-4 h-4">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M14 5l7 7m0 0l-7 7m7-7H3" />
                             </svg>
-                        </motion.button>
+                        </button>
                     </div>
                 </div>
             </div>
