6dc128d5626403c09879d08d2df886232ec52892
mariech5071 <125594493+mariech5071@users.noreply.github.com>
2024-01-16 12:19:14 -0600

    Margins adjusted

--- digital-portfolio/Components/Projects1.tsx
+++ digital-portfolio/Components/Projects1.tsx
@@ -11,7 +11,7 @@ const Projects1 = () => {
             </h1>
             <div className="flex flex-col sm:flex-row sm:space-x-20 justify-center items-center space-y-8 sm:space-y-0 pb-6">
                 <div className="w-full sm:w-1/2 xl:w-1/4" id='img1'>
-                <motion.img whileHover={{ scale: 1.1 }} src="/images/Arcane-studio.png" alt="Project 1" className="w-full h-48 sm:h-48 object-contain mx-auto rounded-lg"/>
+                <motion.img whileHover={{ scale: 1.1 }} src="/images/Arcane-studio.png" alt="Project 1" className="w-full h-48 sm:h-48 object-contain mx-3 rounded-lg"/>
                     <div className="flex items-center justify-between">
                     <p className="font-medium mt-3">
                     Here is project 1
@@ -24,10 +24,10 @@ const Projects1 = () => {
                     </div>
                 </div>
                 <div className="w-full sm:w-1/2 xl:w-1/4" id='img2'>
-                <motion.img whileHover={{ scale: 1.1 }} src="/images/Arcane-studio.png" alt="Project 1" className="w-full h-48 sm:h-48 object-contain mx-auto rounded-lg"/>
+                <motion.img whileHover={{ scale: 1.1 }} src="/images/Arcane-studio.png" alt="Project 1" className="w-full h-48 sm:h-48 object-contain mx-3 rounded-lg"/>
                     <div className="flex items-center justify-between">
                     <p className="font-medium mt-3">
-                    Here is project 1
+                    Landing page for Arcane Studios
                     </p>
                         <button className="w-8 h-8 bg-white rounded-full flex items-center justify-center">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="black" className="w-4 h-4">
@@ -37,7 +37,7 @@ const Projects1 = () => {
                     </div>
                 </div>
                 <div className="w-full sm:w-1/2 xl:w-1/4" id='img3'>
-                <motion.img whileHover={{ scale: 1.1 }} src="/images/Arcane-studio.png" alt="Project 1" className="w-full h-48 sm:h-48 object-contain mx-auto rounded-lg"/>
+                <motion.img whileHover={{ scale: 1.1 }} src="/images/Arcane-studio.png" alt="Project 1" className="w-full h-48 sm:h-48 object-contain mx-3 rounded-lg"/>
                     <div className="flex items-center justify-between">
                     <p className="font-medium mt-3">
                     Here is project 1
--- digital-portfolio/app/Projects/page.tsx
+++ digital-portfolio/app/Projects/page.tsx
@@ -54,10 +54,10 @@ const Projects = () => {
                 <div id='projects' className='bg-custom-orange-lighter min-h-screen flex justify-center items-center flex-col'>
                 <div className="flex flex-col sm:flex-row sm:space-x-20 justify-center items-center space-y-8 sm:space-y-0 pb-3">
                     <div className="w-full sm:w-1/2 xl:w-1/4" id='img1'>
-                    <motion.img  whileHover={{ scale: 1.1 }} src="/images/Arcane-studio.png" alt="Project 1" className="w-full h-48 sm:h-48 object-contain mx-auto rounded-lg md:mt-0 mt-10" initial={{ opacity: 0 }} animate={{ opacity: 1}} transition={{ duration: 0.5 }} />
+                    <motion.img  whileHover={{ scale: 1.1 }} src="/images/Arcane-studio.png" alt="Project 1" className="w-full h-48 sm:h-48 object-contain mx-auto rounded-lg md:mt-0 mt-10 px-2" initial={{ opacity: 0 }} animate={{ opacity: 1}} transition={{ duration: 0.5 }} />
                         <div className="flex items-center justify-between">
                         <motion.p className="font-medium mt-3" initial={{ opacity: 0 }} animate={{ opacity: 1}} transition={{ duration: 0.5 }}>
-                        Here is project 1
+                        To do app
                         </motion.p>
                            <motion.button className="w-8 h-8 bg-white rounded-full flex items-center justify-center" initial={{ opacity: 0 }} animate={{ opacity: 1}} transition={{ duration: 0.5 }}>
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="black" className="w-4 h-4">
@@ -67,10 +67,10 @@ const Projects = () => {
                         </div>
                     </div>
                     <div className="w-full sm:w-1/2 xl:w-1/4" id='img2'>
-                    <motion.img whileHover={{ scale: 1.1 }} src="/images/Arcane-studio.png" alt="Project 1" className="w-full h-48 sm:h-48 object-contain mx-auto rounded-lg" initial={{ opacity: 0 }} animate={{ opacity: 1}} transition={{ duration: 0.5 }} />
+                    <motion.img whileHover={{ scale: 1.1 }} src="/images/Arcane-studio.png" alt="Project 1" className="w-full h-48 sm:h-48 object-contain mx-auto rounded-lg px-2" initial={{ opacity: 0 }} animate={{ opacity: 1}} transition={{ duration: 0.5 }} />
                         <div className="flex items-center justify-between">
                         <motion.p className="font-medium mt-3" initial={{ opacity: 0 }} animate={{ opacity: 1}} transition={{ duration: 0.5 }}>
-                        Here is project 1
+                        Landing page for Arcane Studio
                         </motion.p>
                             <motion.button className="w-8 h-8 bg-white rounded-full flex items-center justify-center" initial={{ opacity: 0 }} animate={{ opacity: 1}} transition={{ duration: 0.5 }}>
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="black" className="w-4 h-4">
@@ -80,8 +80,8 @@ const Projects = () => {
                         </div>
                     </div>
                     <div className="w-full sm:w-1/2 xl:w-1/4" id='img3'>
-                    <motion.img whileHover={{ scale: 1.1 }} src="/images/Arcane-studio.png" alt="Project 1" className="w-full h-48 sm:h-48 object-contain mx-auto rounded-lg" initial={{ opacity: 0 }} animate={{ opacity: 1}} transition={{ duration: 0.5 }}/>
-                        <div className="flex items-center justify-between">
+                    <motion.img whileHover={{ scale: 1.1 }} src="/images/Arcane-studio.png" alt="Project 1" className="w-full h-48 sm:h-48 object-contain mx-auto rounded-lg px-2" initial={{ opacity: 0 }} animate={{ opacity: 1}} transition={{ duration: 0.5 }}/>
+                        <div className="flex items-center justify-between mb-4">
                         <motion.p className="font-medium mt-3" initial={{ opacity: 0 }} animate={{ opacity: 1}} transition={{ duration: 0.5 }}>
                         Here is project 1
                         </motion.p>
